2c494cd6db5eab3e0284faab5f00df94a8d53b68
CarlaPerezGavilan <A01023033@itesm.mx>
2020-04-09 20:43:41 -0500

    added travis and first version of second problem

---
+++ train_time.cpp
@@ -0,0 +1,138 @@
+
+
+#include <stdio.h> 
+#include <limits.h> 
+#include <iostream>
+  
+
+#define V 9 
+  
+
+int minDistance(int dist[],  
+                bool sptSet[]) 
+{ 
+    int min = INT_MAX, min_index; 
+  
+    for (int v = 0; v < V; v++) 
+        if (sptSet[v] == false && 
+                   dist[v] <= min) 
+            min = dist[v], min_index = v; 
+  
+    return min_index; 
+} 
+  
+
+void printPath(int parent[], int j) 
+{ 
+      
+    if (parent[j] == - 1) 
+        return; 
+  
+    printPath(parent, parent[j]); 
+    printf("%d ", j); 
+} 
+  
+
+void printSolution(int dist[], int target, int first,  
+                      int parent[]) 
+{ 
+    printf("Vertex\tTime\tStations"); 
+    
+    printf("\n%d -> %d \t\t %d\t\t%d ", 
+        first, target, dist[target], first); 
+    printPath(parent, target); 
+
+} 
+  
+
+void dijkstra(int graph[V][V], int begin, int end) 
+{ 
+      
+    // The output array. dist[i] 
+    // will hold the shortest 
+    // distance from src to i 
+    int dist[V];  
+  
+    // sptSet[i] will true if vertex 
+    // i is included / in shortest 
+    // path tree or shortest distance  
+    // from src to i is finalized 
+    bool sptSet[V]; 
+  
+    // Parent array to store 
+    // shortest path tree 
+    int parent[V]; 
+  
+    // Initialize all distances as  
+    // INFINITE and stpSet[] as false 
+    for (int i = 0; i < V; i++) 
+    { 
+        parent[begin] = -1; 
+        dist[i] = INT_MAX; 
+        sptSet[i] = false; 
+    } 
+
+    // Distance of source vertex  
+    // from itself is always 0 
+    dist[begin] = 0; 
+  
+    // Find shortest path 
+    // for all vertices 
+    for (int count = 0; count < V - 1; count++) 
+    { 
+        // Pick the minimum distance 
+        // vertex from the set of 
+        // vertices not yet processed.  
+        // u is always equal to src 
+        // in first iteration. 
+        int u = minDistance(dist, sptSet); 
+  
+        // Mark the picked vertex  
+        // as processed 
+        sptSet[u] = true; 
+           printf("hola\n");
+
+        // Update dist value of the  
+        // adjacent vertices of the 
+        // picked vertex. 
+        for (int v = 0; v < V; v++) 
+  
+            // Update dist[v] only if is 
+            // not in sptSet, there is 
+            // an edge from u to v, and  
+            // total weight of path from 
+            // src to v through u is smaller 
+            // than current value of 
+            // dist[v] 
+            if (!sptSet[v] && graph[u][v] && 
+                dist[u] + graph[u][v] < dist[v]) 
+            { 
+                parent[v] = u; 
+                dist[v] = dist[u] + graph[u][v]; 
+            }  
+    } 
+  
+    // print the constructed 
+    // distance array 
+    printSolution(dist, end, begin, parent); 
+} 
+  
+// Driver Code 
+int main() 
+{ 
+    //  Let us create the example 
+    // graph discussed above 
+    int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0}, 
+                       {4, 0, 8, 0, 0, 0, 0, 11, 0}, 
+                        {0, 8, 0, 7, 0, 4, 0, 0, 2}, 
+                        {0, 0, 7, 0, 9, 14, 0, 0, 0}, 
+                        {0, 0, 0, 9, 0, 10, 0, 0, 0}, 
+                        {0, 0, 4, 0, 10, 0, 2, 0, 0}, 
+                        {0, 0, 0, 14, 0, 2, 0, 1, 6}, 
+                        {8, 11, 0, 0, 0, 0, 1, 0, 7}, 
+                        {0, 0, 2, 0, 0, 0, 6, 7, 0} 
+                    }; 
+  
+    dijkstra(graph, 2, 6); 
+    return 0; 
+}
